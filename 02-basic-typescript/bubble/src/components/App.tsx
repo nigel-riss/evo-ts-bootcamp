a194e99b6b08e31fe8c093450b6cdcfcd92a3669
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2021-04-19 23:10:01 +0200

    Cleanup default app

---
+++ 02-basic-typescript/bubble/src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      
+    }
+  }
+}
+
+export default App;
