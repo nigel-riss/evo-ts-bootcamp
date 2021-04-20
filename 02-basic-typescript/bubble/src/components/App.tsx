85a39414d4104080d7334ad7b0149133433be038
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2021-04-20 03:16:42 +0200

    Add styling and base markup

--- 02-basic-typescript/bubble/src/components/App.tsx
+++ 02-basic-typescript/bubble/src/components/App.tsx
@@ -1,18 +1,54 @@
 import React from 'react';
 import './App.css';
+import { Options } from '../utils/const';
 
-class App extends React.Component {
+type AppState = {
+  isSolved: boolean,
+  isPaused: boolean,
+}
+
+class App extends React.Component<{}, AppState> {
+  private intervalId?: number;
+
   constructor(props: {}) {
     super(props);
 
     this.state = {
-
+      isSolved: false,
+      isPaused: true,
     }
+  }
+
+  componentDidMount() {
+    this.intervalId = window.setInterval(() => {
+      console.log(Date.now());
+    }, Options.ITERATION_DELAY);
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.intervalId);
   }
 
   render() {
     return (<div className="app">
-      <h1>Bubble Sort visualization</h1>
+      <h1 className="app__title">Bubble Sort Visualization</h1>
+      <div className="app__bars"></div>
+      <div className="app__buttons">
+        <button>
+          New Set
+        </button>
+        <button>
+          {this.state.isPaused ? `Start` : `Pause`}
+        </button>
+      </div>
+      <input
+        type="range"
+        name=""
+        id=""
+        min="0"
+        max="10"
+        step="1"
+      />
     </div>)
   }
 }
